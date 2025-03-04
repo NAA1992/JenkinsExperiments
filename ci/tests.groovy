a8621444270b83889b2e582a190d7231527b8f64
Aleksey Nikhaenko <a.nikhaenko@digitalms.ru>
2025-03-04 23:37:13 +0300

    No error! Wow!

--- ci/tests.groovy
+++ ci/tests.groovy
@@ -70,11 +70,11 @@ pipeline {
                     GLOBAL_ENV_BREAK="${params.get('Enable_Breake_Stage')}" // YES
                     echo "GLOBAL_ENV_BREAK is ${GLOBAL_ENV_BREAK}" // YES
                     ONE_MORE_WAY_TO_GET_FROM_PARAM = "$params.get('Enable_Breake_Stage')"
-                    echo "$ONE_MORE_WAY_TO_GET_FROM_PARAM" // printed as null('Enable_Breake_Stage')
+                    echo "$ONE_MORE_WAY_TO_GET_FROM_PARAM" // null('Enable_Breake_Stage')
                     ONE_MORE_WAY_TO_GET_FROM_PARAM = Enable_Breake_Stage
-                    echo "$ONE_MORE_WAY_TO_GET_FROM_PARAM"
+                    echo "$ONE_MORE_WAY_TO_GET_FROM_PARAM" // YES
                     ONE_MORE_WAY_TO_GET_FROM_PARAM = '${params.get("Enable_Breake_Stage")}'
-                    echo "$ONE_MORE_WAY_TO_GET_FROM_PARAM"
+                    echo "$ONE_MORE_WAY_TO_GET_FROM_PARAM" // printed as is
                     echo "Try to create env DEV_SREDA with value from variable shell_param_dev"
                     DEV_SREDA = "${shell_param_dev}" // dev
                     echo "${DEV_SREDA}" // dev
