002441eee82cd149a7cc59d935fa49a09cce6d3c
Aleksey Nikhaenko <a.nikhaenko@digitalms.ru>
2025-03-04 21:17:32 +0300

    TESTS with env, params

--- ci/tests.groovy
+++ ci/tests.groovy
@@ -5,6 +5,9 @@ def AllowedBranchesAsList() {
     PROD_BRANCHES = env.PROD_BRANCHES.split(',').collect { it.trim() } // Убираем пробелы
 }
 
+shell_param_dev = "dev" // глобальная переменная DEV среды () 
+shell_param_prod = "prod"
+
 pipeline {
     agent { label "${AGENT}" }
 
@@ -14,31 +17,60 @@ pipeline {
 
     parameters {
         booleanParam(name: 'checkoutIntoVar', defaultValue: true, description: 'Присваиваем ли переменной checkoutResult результат команды checkout scm?')
-        string(defaultValue: 'dev', description: 'Argument for shell command DEV stand', name: 'shell_param_dev', trim: true)
-        string(defaultValue: 'prod', description: 'Argument for shell command PROD stand', name: 'shell_param_prod', trim: true)
+        string(name: 'Enable_Breake_Stage', defaultValue: 'YES', description: 'Enable Break Stage', trim: true)
     }
 
     environment {
         ENV_FILE=".env.example"
         DEVELOPMENT_BRANCHES = "development, copy-jenkins-branch" // можно указывать через запятую, например "test, dev, qa"
         PROD_BRANCHES = 'main'
+        TRY_TO_CHANGE_ME = 'DEFAULT_VALUE'
     }
 
     stages {
+        stage('Environments') {
+            when {
+                expression {"${currentBuild.currentResult}" == 'SUCCESS'}
+            }
+            steps {
+                script {
+                    echo "Try print each ENV_NAME, ENV_VALUE"
+                    env.each { env_key, env_value ->
+                        echo "Environment variable: ${env_key} = ${env_value}"
+                    }
+                    echo "Try get value by name. Example, HOME"
+                    HOME_VAR = env.get("HOME")
+                    echo "${HOME_VAR}"
+                    echo "Try change env TRY_TO_CHANGE_ME"
+                    env.TRY_TO_CHANGE_ME = 'CHANGED_VALUE'
+                    echo "${env.TRY_TO_CHANGE_ME}"
+                    echo "${TRY_TO_CHANGE_ME}"
+                    echo "Try create global env GLOBAL_ENV_BREAK with value from param Enable_Breake_Stage"
+                    GLOBAL_ENV_BREAK="${params.get('Enable_Breake_Stage')}"
+                    echo "${GLOBAL_ENV_BREAK}"
+                    echo "Try to create env DEV_SREDA with value from variable shell_param_dev"
+                    DEV_SREDA = "${shell_param_dev}"
+                    echo "${DEV_SREDA}"
+                    echo "Original value shell_param_dev is ${shell_param_dev}"
+                    echo "Print NOT_EXISTS env: ${NOT_EXISTS}"
+                    echo "Print env.NOT_EXISTS env: ${env.NOT_EXISTS}"
+                    echo sh(script: 'env|sort', returnStdout: true)
+                }
+            }
+        }
         stage('Print params') {
             when {
                 expression {"${currentBuild.currentResult}" == 'SUCCESS'}
             }
             steps {
                 script {
-                    echo "Create var shell_param (value = shell_param_dev)"
-                    SHELL_PARAM=params.get('shell_param_dev')
-                    echo "${SHELL_PARAM}"
-                    echo "Directly get param (checkoutIntoVar)"
-                    echo "${params.get('checkoutIntoVar')}"
+                    echo "Check, that environments from previous stage is saved"
+                    echo "GLOBAL_ENV_BREAK = ${GLOBAL_ENV_BREAK}"
+                    echo "env.TRY_TO_CHANGE_ME  = ${env.TRY_TO_CHANGE_ME}"
+                    echo "TRY_TO_CHANGE_ME = ${TRY_TO_CHANGE_ME}"
                     echo "Each param print"
                     params.each { param_key, param_value ->
-                        echo "Ключ: ${param_key}, Значение: ${param_value}"
+                        echo "Param Key: ${param_key}, Param Value: ${param_value}"
                     }
                     /* Other way to print each param
                     params.each {param ->
@@ -46,44 +78,10 @@ pipeline {
                         echo "param.value: ${param.value}"
                     }
                     */
-                }
-            }
-        }
-        stage('Set New Env (ME_EXISTS)') {
-            when {
-                anyOf {
-                    expression { params.checkoutIntoVar == true }
-                    expression { currentBuild.currentResult == 'SUCCESS' }
-                }
-            }
-            steps {
-                script {
-                        env.ME_EXISTS='YES, OF COURSE'
-                    }
-                    
-                }
-        }
-        stage('PrAllEnv') {
-            when {
-                anyOf {
-                    expression { currentBuild.result == 'SUCCESS' } // here is null
-                    expression { currentBuild.currentResult == 'SUCCESS' }
-                }
-            }
-            steps {
-                script {
-                    echo "Print from previous stage var shell_param"
-                    sh "echo ${SHELL_PARAM}"
-                    echo "Print from previos ENV var (ME_EXISTS)"
-                    echo "NOT EXISTS IS ${env.ME_EXISTS}"
-                    echo "Try print environment var, who is not exists"
-                    echo "NOT EXISTS IS ${env.ME_NOT_EXISTS}"
-                    echo "print all env"
                     echo sh(script: 'env|sort', returnStdout: true)
                 }
             }
         }
-        
         stage('CheckoutSCM into var') {
             when {
                 allOf {
@@ -107,9 +105,11 @@ pipeline {
                     try {
                         if (gitlabTargetBranch == null) {
                             gitlabTargetBranch = env.GIT_BRANCH
+                            echo "gitlabTargetBranch was null"
                         }
                     } catch (MissingPropertyException e) {
                         gitlabTargetBranch = env.GIT_BRANCH
+                        echo "gitlabTargetBranch was NOT EXISTS"
                         }
                     catchError(buildResult: 'ABORTED', stageResult: 'ABORTED') {
                         def gitlabTargetBranch = gitlabTargetBranch.split("/")[-1]
@@ -132,12 +132,13 @@ pipeline {
         }
         stage('Status ABORT') {
             when {
-                expression {"${currentBuild.currentResult}" == 'SUCCESS'}
+                allOf {
+                    expression { params.Enable_Breake_Stage == 'YES' }
+                    expression { currentBuild.currentResult == 'SUCCESS' }
+                }
             }
             steps {
                 script {
-                    echo "Try print shell_param"
-                    sh "echo ${SHELL_PARAM}"
                     catchError(buildResult: 'ABORTED', stageResult: 'ABORTED') {
                         if (1==1) {
                             error('Next прерван, выполнение остановлено.')
@@ -146,7 +147,7 @@ pipeline {
                 }
             }
         }
-        stage('It stage forever skip') {
+        stage('It stage skip if status abort') {
             when {
                 expression {"${currentBuild.currentResult}" == 'SUCCESS'}
             }
